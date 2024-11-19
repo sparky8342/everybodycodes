fa575a3d86fcdb136420431d62389a837481a8a8
David Bell <david.bell@gmail.com>
2024-11-19 18:10:44 +0000

    added tests

---
+++ events/2024/quests/quest1/quest1_test.go
@@ -0,0 +1,35 @@
+package quest1
+
+import (
+	"testing"
+)
+
+func Test1(t *testing.T) {
+	data := []byte("ABBAC")
+	got := calculate_potions(data)
+	want := 5
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func Test2(t *testing.T) {
+	data := []byte("AxBCDDCAxD")
+	got := calculate_potions_pairs(data)
+	want := 28
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func Test3(t *testing.T) {
+	data := []byte("xBxAAABCDxCC")
+	got := calculate_potions_triples(data)
+	want := 30
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
