4d175188300b55f77547535d244ad53a4eb64714
David Bell <david.bell@gmail.com>
2024-11-22 02:04:05 +0000

    quest 8 part 2

--- events/2024/quests/quest8/quest8.go
+++ events/2024/quests/quest8/quest8.go
@@ -17,13 +17,32 @@ func build(blocks int) int {
 	return (blocks_used - blocks) * triangle_size
 }
 
+func build_part2(blocks int, multiplier int, mod int) int {
+	width := 1
+	height := 1
+	blocks_used := 1
+
+	for blocks_used < blocks {
+		width += 2
+		height = (height * multiplier) % mod
+		blocks_used += width * height
+	}
+
+	return (blocks_used - blocks) * width
+}
+
 func Run() {
 	loader.Event, loader.Quest, loader.Part = "2024", 8, 1
 
 	blocks := loader.GetOneInt()
 	part1 := build(blocks)
 
-	part2 := -1
+	loader.Part = 2
+	blocks = 20240000
+	multiplier := loader.GetOneInt()
+	mod := 1111
+	part2 := build_part2(blocks, multiplier, mod)
+
 	part3 := -1
 	fmt.Printf("%d %d %d\n", part1, part2, part3)
 }
--- events/2024/quests/quest8/quest8_test.go
+++ events/2024/quests/quest8/quest8_test.go
@@ -14,3 +14,16 @@ func Test1(t *testing.T) {
 		t.Errorf("got %d, wanted %d", got, want)
 	}
 }
+
+func Test2(t *testing.T) {
+	multiplier := 3
+	mod := 5
+	blocks := 50
+
+	got := build_part2(blocks, multiplier, mod)
+	want := 27
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
