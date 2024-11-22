249ebf4f11d8425cfef081379fc473da90c2d2ab
David Bell <david.bell@gmail.com>
2024-11-22 02:42:57 +0000

    quest 8 part 3

--- events/2024/quests/quest8/quest8.go
+++ events/2024/quests/quest8/quest8.go
@@ -31,6 +31,38 @@ func build_part2(blocks int, multiplier int, mod int) int {
 	return (blocks_used - blocks) * width
 }
 
+func build_part3(blocks int, multiplier int, mod int) int {
+	width := 1
+	height_to_add := 1
+	heights := []int{1}
+
+	for {
+		width += 2
+		height_to_add = ((height_to_add * multiplier) % mod) + mod
+		for i := 0; i < len(heights); i++ {
+			heights[i] += height_to_add
+		}
+		heights = append(heights, height_to_add)
+
+		// central column
+		blocks_used := heights[0]
+		blocks_used -= (multiplier * width * heights[0]) % mod
+
+		// end columns
+		blocks_used += heights[len(heights)-1] * 2
+
+		// all other columns
+		for i := 1; i < len(heights)-1; i++ {
+			blocks_used += heights[i] * 2
+			blocks_used -= ((multiplier * width * heights[i]) % mod) * 2
+		}
+
+		if blocks_used > blocks {
+			return blocks_used - blocks
+		}
+	}
+}
+
 func Run() {
 	loader.Event, loader.Quest, loader.Part = "2024", 8, 1
 
@@ -43,6 +75,11 @@ func Run() {
 	mod := 1111
 	part2 := build_part2(blocks, multiplier, mod)
 
-	part3 := -1
+	loader.Part = 3
+	blocks = 202400000
+	multiplier = loader.GetOneInt()
+	mod = 10
+	part3 := build_part3(blocks, multiplier, mod)
+
 	fmt.Printf("%d %d %d\n", part1, part2, part3)
 }
--- events/2024/quests/quest8/quest8_test.go
+++ events/2024/quests/quest8/quest8_test.go
@@ -27,3 +27,16 @@ func Test2(t *testing.T) {
 		t.Errorf("got %d, wanted %d", got, want)
 	}
 }
+
+func Test3(t *testing.T) {
+	multiplier := 2
+	mod := 5
+	blocks := 160
+
+	got := build_part3(blocks, multiplier, mod)
+	want := 2
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
