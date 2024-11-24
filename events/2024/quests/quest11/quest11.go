be30dd3426cca993920771b3fa063fd9f242876f
David Bell <david.bell@gmail.com>
2024-11-24 03:30:14 +0000

    quest 11 part 3

--- events/2024/quests/quest11/quest11.go
+++ events/2024/quests/quest11/quest11.go
@@ -3,48 +3,55 @@ package quest11
 import (
 	"fmt"
 	"loader"
+	"math"
 	"strings"
 )
 
-func parse_data(data []string) [][]int {
-	rules := make([][]int, 26)
-
+func parse_data(data []string) map[string][]string {
+	rules := map[string][]string{}
 	for _, line := range data {
 		parts := strings.Split(line, ":")
 		source := parts[0]
-		destination := []int{}
-		for _, dest := range strings.Split(parts[1], ",") {
-			destination = append(destination, int(dest[0]-65))
-		}
-		rules[int(source[0]-65)] = destination
+		rules[source] = strings.Split(parts[1], ",")
 	}
-
 	return rules
 }
 
-func day(rules [][]int, population []int) []int {
-	next := make([]int, 26)
-	for i := 0; i < 26; i++ {
-		if population[i] > 0 {
-			for _, n := range rules[i] {
-				next[n] += population[i]
-			}
+func day(rules map[string][]string, population map[string]uint) map[string]uint {
+	next := map[string]uint{}
+	for typ, amount := range population {
+		for _, dest := range rules[typ] {
+			next[dest] += amount
 		}
 	}
 	return next
 }
 
-func days(rules [][]int, start int, amount int) int {
-	population := make([]int, 26)
+func days(rules map[string][]string, start string, amount int) uint {
+	population := map[string]uint{}
 	population[start] = 1
 	for i := 0; i < amount; i++ {
 		population = day(rules, population)
 	}
-	count := 0
-	for i := 0; i < 26; i++ {
-		count += population[i]
+	var count uint = 0
+	for _, n := range population {
+		count += n
 	}
 	return count
+}
+
+func all_starts(rules map[string][]string) uint {
+	var min uint = math.MaxInt64
+	var max uint = 0
+	for source := range rules {
+		count := days(rules, source, 20)
+		if count < min {
+			min = count
+		} else if count > max {
+			max = count
+		}
+	}
+	return max - min
 }
 
 func Run() {
@@ -52,13 +59,17 @@ func Run() {
 
 	data := loader.GetStrings()
 	rules := parse_data(data)
-	part1 := days(rules, 0, 4)
+	part1 := days(rules, "A", 4)
 
 	loader.Part = 2
 	data = loader.GetStrings()
 	rules = parse_data(data)
-	part2 := days(rules, 25, 10)
+	part2 := days(rules, "Z", 10)
 
-	part3 := -1
+	loader.Part = 3
+	data = loader.GetStrings()
+	rules = parse_data(data)
+	part3 := all_starts(rules)
+
 	fmt.Printf("%d %d %d\n", part1, part2, part3)
 }
--- events/2024/quests/quest11/quest11_test.go
+++ events/2024/quests/quest11/quest11_test.go
@@ -12,8 +12,24 @@ func Test1(t *testing.T) {
 	}
 
 	rules := parse_data(data)
-	got := days(rules, 0, 4)
-	want := 8
+	got := days(rules, "A", 4)
+	var want uint = 8
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func Test2(t *testing.T) {
+	data := []string{
+		"A:B,C",
+		"B:C,A,A",
+		"C:A",
+	}
+
+	rules := parse_data(data)
+	got := all_starts(rules)
+	var want uint = 268815
 
 	if got != want {
 		t.Errorf("got %d, wanted %d", got, want)
