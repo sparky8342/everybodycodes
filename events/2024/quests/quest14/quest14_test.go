49b424adf5d4aeabb0ab40b5f27bc0ea19bd72fe
David Bell <david.bell@gmail.com>
2024-11-26 16:57:46 +0000

    added test

--- events/2024/quests/quest14/quest14_test.go
+++ events/2024/quests/quest14/quest14_test.go
@@ -42,3 +42,20 @@ func Test3(t *testing.T) {
 		t.Errorf("got %d, wanted %d", got, want)
 	}
 }
+
+func Test4(t *testing.T) {
+	data := []string{
+		"U20,L1,B1,L2,B1,R2,L1,F1,U1",
+		"U10,F1,B1,R1,L1,B1,L1,F1,R2,U1",
+		"U30,L2,F1,R1,B1,R1,F2,U1,F1",
+		"U25,R1,L2,B1,U1,R2,F1,L2",
+		"U16,L1,B1,L1,B3,L1,B1,F1",
+	}
+
+	got := murkiness(data)
+	want := 46
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
