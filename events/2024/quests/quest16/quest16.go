e227bd9cd999301125e4a5ef7e35c2986d7299fd
David Bell <david.bell@gmail.com>
2024-11-27 16:32:22 +0000

    quest 16 part 2

--- events/2024/quests/quest16/quest16.go
+++ events/2024/quests/quest16/quest16.go
@@ -45,10 +45,78 @@ func parse_data(data []string) Machine {
 	return machine
 }
 
+func (m *Machine) reset() {
+	for i := 0; i < len(m.positions); i++ {
+		m.positions[i] = 0
+	}
+}
+
 func (m *Machine) spin(amount int) {
 	for i := 0; i < len(m.positions); i++ {
 		m.positions[i] = (m.positions[i] + m.rotations[i]*amount) % len(m.wheels[i])
 	}
+}
+
+func (m *Machine) score() int {
+	symbols := map[byte]int{}
+	for i := 0; i < len(m.wheels); i++ {
+		symbols[m.wheels[i][m.positions[i]][0]]++
+		symbols[m.wheels[i][m.positions[i]][2]]++
+	}
+	score := 0
+	for _, count := range symbols {
+		if count >= 3 {
+			score += count - 2
+		}
+	}
+	return score
+}
+
+func (m *Machine) spin_with_score(amount int) int {
+	total := 0
+	sequences := map[string][]int{}
+
+	for i := 0; i < amount; i++ {
+		for j := 0; j < len(m.positions); j++ {
+			m.positions[j] = (m.positions[j] + m.rotations[j]) % len(m.wheels[j])
+		}
+
+		symbols := map[byte]int{}
+		for i := 0; i < len(m.wheels); i++ {
+			symbols[m.wheels[i][m.positions[i]][0]]++
+			symbols[m.wheels[i][m.positions[i]][2]]++
+		}
+		score := 0
+		for _, count := range symbols {
+			if count >= 3 {
+				score += count - 2
+			}
+		}
+		total += score
+
+		pos := make([]string, len(m.positions))
+		for i, position := range m.positions {
+			pos[i] = strconv.Itoa(position)
+		}
+		state := strings.Join(pos, "")
+
+		if last, ok := sequences[state]; ok {
+			diff := total - last[1]
+
+			left := amount - i
+			step := i - last[0]
+			steps := left / step
+
+			total += diff * steps
+			i += steps * step
+			sequences = map[string][]int{}
+		} else {
+			sequences[state] = []int{i, total}
+		}
+
+	}
+
+	return total
 }
 
 func (m *Machine) display() string {
@@ -67,6 +135,11 @@ func Run() {
 	machine.spin(100)
 	part1 := machine.display()
 
-	part2, part3 := -1, -1
+	loader.Part = 2
+	data = loader.GetStrings()
+	machine = parse_data(data)
+	part2 := machine.spin_with_score(202420242024)
+
+	part3 := -1
 	fmt.Printf("%s %d %d\n", part1, part2, part3)
 }
--- events/2024/quests/quest16/quest16_test.go
+++ events/2024/quests/quest16/quest16_test.go
@@ -25,3 +25,45 @@ func Test1(t *testing.T) {
 		t.Errorf("got %s, wanted %s", got, want)
 	}
 }
+
+func Test2(t *testing.T) {
+	data := []string{
+		"1,2,3",
+		"",
+		"^_^ -.- ^,-",
+		">.- ^_^ >.<",
+		"-_- -.- >.<",
+		"    -.^ ^_^",
+		"    >.>",
+	}
+
+	machine := parse_data(data)
+
+	got := machine.spin_with_score(10)
+	want := 15
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+
+	machine.reset()
+	got = machine.spin_with_score(100)
+	want = 138
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+
+	machine.reset()
+	got = machine.spin_with_score(10000000000)
+	want = 13833333333
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+
+	machine.reset()
+	got = machine.spin_with_score(202420242024)
+	want = 280014668134
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+
+}
