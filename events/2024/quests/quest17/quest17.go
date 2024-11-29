77a7658b733f89e0cc57a59fc7daa5b1726814bf
David Bell <david.bell@gmail.com>
2024-11-29 19:09:10 +0000

    quest 17 part 3

--- events/2024/quests/quest17/quest17.go
+++ events/2024/quests/quest17/quest17.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"loader"
 	"math"
+	"sort"
 )
 
 type Star struct {
@@ -89,6 +90,56 @@ func minimum_spanning_tree(stars []Star) int {
 	return total + len(stars)
 }
 
+func constellations(stars []Star) int {
+	distances := compute_distances(stars)
+
+	// Prim's algorithm
+	available := map[int]struct{}{}
+	for i := 0; i < len(stars); i++ {
+		available[i] = struct{}{}
+	}
+
+	sizes := []int{}
+
+	for len(available) > 0 {
+		total := 0
+		vertices := []int{}
+		for n := range available {
+			vertices = []int{n}
+			delete(available, n)
+			break
+		}
+
+		for len(available) > 0 {
+			min := math.MaxInt32
+			closest := 0
+			for _, vertex := range vertices {
+				for vertex2 := range available {
+					dist := distances[vertex][vertex2]
+					if dist < min {
+						min = dist
+						closest = vertex2
+					}
+				}
+			}
+			if min >= 6 {
+				break
+			}
+			delete(available, closest)
+			vertices = append(vertices, closest)
+			total += min
+		}
+
+		sizes = append(sizes, total+len(vertices))
+	}
+
+	sort.Slice(sizes, func(i, j int) bool {
+		return sizes[i] > sizes[j]
+	})
+
+	return sizes[0] * sizes[1] * sizes[2]
+}
+
 func Run() {
 	loader.Event, loader.Quest, loader.Part = "2024", 17, 1
 
@@ -101,6 +152,10 @@ func Run() {
 	stars = parse_data(data)
 	part2 := minimum_spanning_tree(stars)
 
-	part3 := -1
+	loader.Part = 3
+	data = loader.GetStrings()
+	stars = parse_data(data)
+	part3 := constellations(stars)
+
 	fmt.Printf("%d %d %d\n", part1, part2, part3)
 }
