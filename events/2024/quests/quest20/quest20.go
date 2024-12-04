82192870cbd4bc7257bff18c0f9555fec7edf27c
David Bell <david.bell@gmail.com>
2024-12-04 01:53:00 +0000

    quest 20 part 2

--- events/2024/quests/quest20/quest20.go
+++ events/2024/quests/quest20/quest20.go
@@ -6,14 +6,22 @@ import (
 )
 
 type Glider struct {
-	x        int
-	y        int
+	x        uint8
+	y        uint8
 	dir      byte
 	altitude int
 	time     int
+	letters  int
 }
 
-var height, width int
+type State struct {
+	x       uint8
+	y       uint8
+	dir     byte
+	letters int
+}
+
+var height, width uint8
 
 func dfs(grid []string, glider Glider, visited map[Glider]struct{}, max *int) {
 	if glider.time == 100 {
@@ -50,7 +58,7 @@ func dfs(grid []string, glider Glider, visited map[Glider]struct{}, max *int) {
 			new_x++
 		}
 
-		if new_x < 0 || new_x == width || new_y < 0 || new_y == height {
+		if new_x == 255 || new_x == width || new_y == 255 || new_y == height {
 			continue
 		}
 		if grid[new_y][new_x] == '#' {
@@ -83,8 +91,8 @@ func dfs(grid []string, glider Glider, visited map[Glider]struct{}, max *int) {
 }
 
 func highest_altitude(grid []string) int {
-	height = len(grid)
-	width = len(grid[0])
+	height = uint8(len(grid))
+	width = uint8(len(grid[0]))
 
 	start_x := width / 2
 
@@ -102,7 +110,116 @@ func highest_altitude(grid []string) int {
 	}
 
 	return max
+}
 
+func state_key(glider Glider) int {
+	return ((((((int(glider.x) * 256) + int(glider.y)) * 256) + int(glider.dir-'A')) * 10) + glider.letters)
+}
+
+func shortest_path(grid []string) int {
+	height = uint8(len(grid))
+	width = uint8(len(grid[0]))
+
+	start_x := width / 2
+
+	start_glider := Glider{x: start_x, y: 0, dir: 'D', altitude: 10000, time: 0}
+	queue := []Glider{start_glider}
+
+	visited := map[int]int{}
+
+	for len(queue) > 0 {
+		glider := queue[0]
+		queue = queue[1:]
+
+		if glider.x == start_x && glider.y == 0 && glider.letters == 7 && glider.altitude >= 10000 {
+			return glider.time
+		}
+
+		var directions []byte
+		switch glider.dir {
+		case 'U':
+			directions = []byte{'U', 'L', 'R'}
+		case 'D':
+			directions = []byte{'D', 'L', 'R'}
+		case 'L':
+			directions = []byte{'L', 'U', 'D'}
+		case 'R':
+			directions = []byte{'R', 'U', 'D'}
+		}
+
+		for _, dir := range directions {
+			new_x := glider.x
+			new_y := glider.y
+
+			switch dir {
+			case 'U':
+				new_y--
+			case 'D':
+				new_y++
+			case 'L':
+				new_x--
+			case 'R':
+				new_x++
+			}
+
+			if new_x == 255 || new_x == width || new_y == 255 || new_y == height {
+				continue
+			}
+			if grid[new_y][new_x] == '#' {
+				continue
+			}
+
+			altitude := glider.altitude
+			letters := glider.letters
+			switch grid[new_y][new_x] {
+			case '.':
+				altitude--
+			case '-':
+				altitude -= 2
+			case '+':
+				altitude++
+			case 'A':
+				altitude--
+				letters |= 1
+			case 'B':
+				if letters&1 == 0 {
+					continue
+				}
+				altitude--
+				letters |= 2
+			case 'C':
+				if letters&1 == 0 || letters&2 == 0 {
+					continue
+				}
+				altitude--
+				letters |= 4
+			case 'S':
+				altitude--
+			}
+
+			new_glider := Glider{
+				x:        new_x,
+				y:        new_y,
+				dir:      dir,
+				altitude: altitude,
+				time:     glider.time + 1,
+				letters:  letters,
+			}
+
+			key := state_key(new_glider)
+			if val, ok := visited[key]; ok {
+				if val >= altitude {
+					continue
+				}
+			}
+
+			queue = append(queue, new_glider)
+			visited[key] = altitude
+
+		}
+	}
+
+	return -1
 }
 
 func Run() {
@@ -111,6 +228,10 @@ func Run() {
 	grid := loader.GetStrings()
 	part1 := highest_altitude(grid)
 
-	part2, part3 := -1, -1
+	loader.Part = 2
+	grid = loader.GetStrings()
+	part2 := shortest_path(grid)
+
+	part3 := -1
 	fmt.Printf("%d %d %d\n", part1, part2, part3)
 }
