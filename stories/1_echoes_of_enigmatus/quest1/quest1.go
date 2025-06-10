ab30ce1f92332b0489c37de03cd0363fcfb61789
David Bell <david.bell@gmail.com>
2025-06-10 16:40:41 +0100

    story 1 quest 2 part 3

--- stories/1_echoes_of_enigmatus/quest1/quest1.go
+++ stories/1_echoes_of_enigmatus/quest1/quest1.go
@@ -82,6 +82,63 @@ func eni2(n int, exp int, mod int) int {
 	return result
 }
 
+func sequence_doubles(nums []int) bool {
+	if len(nums)%2 == 1 {
+		return false
+	}
+	l := len(nums) / 2
+	for i := 0; i < l; i++ {
+		if nums[i] != nums[i+l] {
+			return false
+		}
+	}
+	return true
+}
+
+func find_sequence(nums []int) (bool, []int) {
+	l := len(nums)
+
+	for i := 0; i < l; i++ {
+		if sequence_doubles(nums[i:l]) {
+			return true, nums[i+((l-i)/2):]
+		}
+	}
+
+	return false, []int{}
+}
+
+func eni3(n int, exp int, mod int) int {
+	remainders := []int{}
+	num := 1
+
+	total := 0
+
+	var found bool
+	var seq []int
+	for {
+		num = (num * n) % mod
+		exp--
+		total += num
+		remainders = append(remainders, num)
+		found, seq = find_sequence(remainders)
+		if found {
+			break
+		}
+	}
+
+	seq_total := 0
+	for _, rem := range seq {
+		seq_total += rem
+	}
+
+	total += (exp / len(seq)) * seq_total
+	for i := 0; i < exp%len(seq); i++ {
+		total += seq[i]
+	}
+
+	return total
+}
+
 func highest_line(param_lines []map[byte]int, eni func(n int, exp int, mod int) int) int {
 	max := 0
 	for _, params := range param_lines {
@@ -106,7 +163,10 @@ func Run() {
 	param_lines = parse_data(data)
 	part2 := highest_line(param_lines, eni2)
 
-	part3 := -1
+	loader.Part = 3
+	data = loader.GetStrings()
+	param_lines = parse_data(data)
+	part3 := highest_line(param_lines, eni3)
 
 	fmt.Printf("%d %d %d\n", part1, part2, part3)
 }
--- stories/1_echoes_of_enigmatus/quest1/quest1_test.go
+++ stories/1_echoes_of_enigmatus/quest1/quest1_test.go
@@ -62,3 +62,42 @@ func Test3(t *testing.T) {
 		t.Errorf("got %d, wanted %d", got, want)
 	}
 }
+
+func Test4(t *testing.T) {
+	data := []string{
+		"A=4 B=4 C=6 X=3000 Y=14000 Z=15000 M=110",
+		"A=8 B=4 C=7 X=8000 Y=14000 Z=16000 M=120",
+		"A=2 B=8 C=6 X=2000 Y=14000 Z=15000 M=130",
+		"A=5 B=9 C=6 X=8000 Y=16000 Z=18000 M=140",
+		"A=5 B=9 C=7 X=6000 Y=16000 Z=18000 M=150",
+		"A=8 B=8 C=8 X=6000 Y=19000 Z=16000 M=160",
+	}
+
+	param_lines := parse_data(data)
+
+	got := highest_line(param_lines, eni3)
+	want := 3279640
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func Test5(t *testing.T) {
+	data := []string{
+		"A=3657 B=3583 C=9716 X=903056852 Y=9283895500 Z=85920867478 M=188",
+		"A=6061 B=4425 C=5082 X=731145782 Y=1550090416 Z=87586428967 M=107",
+		"A=7818 B=5395 C=9975 X=122388873 Y=4093041057 Z=58606045432 M=102",
+		"A=7681 B=9603 C=5681 X=716116871 Y=6421884967 Z=66298999264 M=196",
+		"A=7334 B=9016 C=8524 X=297284338 Y=1565962337 Z=86750102612 M=145",
+	}
+
+	param_lines := parse_data(data)
+
+	got := highest_line(param_lines, eni3)
+	want := 7276515438396
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
