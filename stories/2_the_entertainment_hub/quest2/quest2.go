7ae01be940a049ae9f55f532a09d86dc3a48e642
David Bell[C <david.bell@gmail.com>
2025-08-28 00:24:43 +0100

    story 2 quest 2 part 2

--- stories/2_the_entertainment_hub/quest2/quest2.go
+++ stories/2_the_entertainment_hub/quest2/quest2.go
@@ -5,6 +5,79 @@ import (
 	"loader"
 )
 
+type Balloon struct {
+	colour byte
+	prev   *Balloon
+	next   *Balloon
+}
+
+func print_circle(head *Balloon) {
+	balloon := head
+	for balloon.next != head {
+		fmt.Print(string(balloon.colour) + " ")
+		balloon = balloon.next
+	}
+	fmt.Println(string(balloon.colour))
+}
+
+func shoot_balloon_circle(balloons []byte, repeat int) int {
+	bolts := []byte{'R', 'G', 'B'}
+	bolt_i := 0
+
+	dummy_head := &Balloon{}
+	balloon := dummy_head
+	for i := 0; i < repeat; i++ {
+		for _, colour := range balloons {
+			new_balloon := &Balloon{colour: colour, prev: balloon}
+			balloon.next = new_balloon
+			balloon = new_balloon
+		}
+	}
+	head := dummy_head.next
+	balloon.next = head
+	head.prev = balloon
+
+	no_balloons := len(balloons) * repeat
+	opposite := head
+	for i := 0; i < no_balloons/2; i++ {
+		opposite = opposite.next
+	}
+
+	shots := 0
+
+	for no_balloons > 0 {
+		current_colour := head.colour
+
+		prev := head.prev
+		next := head.next
+		prev.next = next
+		next.prev = prev
+		head.next = nil
+		head.prev = nil
+		head = next
+
+		no_balloons--
+
+		if no_balloons&1 == 1 && current_colour == bolts[bolt_i] {
+			prev := opposite.prev
+			next := opposite.next
+			prev.next = next
+			next.prev = prev
+			opposite.next = nil
+			opposite.prev = nil
+			opposite = next
+			no_balloons--
+		} else if no_balloons&1 == 0 {
+			opposite = opposite.next
+		}
+
+		shots++
+		bolt_i = (bolt_i + 1) % len(bolts)
+	}
+
+	return shots
+}
+
 func shoot_balloons(balloons []byte) int {
 	bolts := []byte{'R', 'G', 'B'}
 	bolt_i := 0
@@ -34,6 +107,10 @@ func Run() {
 	balloons := loader.GetOneLine()
 	part1 := shoot_balloons(balloons)
 
-	fmt.Printf("%d\n", part1)
+	loader.Part = 2
+	balloons = loader.GetOneLine()
+	part2 := shoot_balloon_circle(balloons, 100)
+
+	fmt.Printf("%d\n%d\n", part1, part2)
 
 }
--- stories/2_the_entertainment_hub/quest2/quest2_test.go
+++ stories/2_the_entertainment_hub/quest2/quest2_test.go
@@ -14,3 +14,14 @@ func Test1(t *testing.T) {
 		t.Errorf("got %d, wanted %d", got, want)
 	}
 }
+
+func Test2(t *testing.T) {
+	balloons := []byte("GGBR")
+
+	got := shoot_balloon_circle(balloons, 5)
+	want := 14
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
