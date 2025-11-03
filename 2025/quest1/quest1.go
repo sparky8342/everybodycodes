ebe8ce76157839f9656418daef09e455c1848fd2
David Bell[C <david.bell@gmail.com>
2025-11-03 23:59:26 +0000

    2025 quest 1 part 2

--- 2025/quest1/quest1.go
+++ 2025/quest1/quest1.go
@@ -3,26 +3,35 @@ package quest1
 import (
 	"fmt"
 	"loader"
+	"strconv"
 	"strings"
 )
 
-func get_name(data []string) string {
+func get_name(data []string, circular bool) string {
 	names := strings.Split(data[0], ",")
 	instructions := strings.Split(data[2], ",")
 
 	pos := 0
 	for _, ins := range instructions {
-		n := int(ins[1] - '0')
+		n, err := strconv.Atoi(ins[1:])
+		if err != nil {
+			panic(err)
+		}
+
 		if ins[0] == 'L' {
 			pos -= n
-			if pos < 0 {
-				pos = 0
-			}
 		} else if ins[0] == 'R' {
 			pos += n
-			if pos >= len(names) {
-				pos = len(names) - 1
+		}
+		if circular {
+			pos = pos % len(names)
+			for pos < 0 {
+				pos += len(names)
 			}
+		} else if pos < 0 {
+			pos = 0
+		} else if pos >= len(names) {
+			pos = len(names) - 1
 		}
 	}
 
@@ -33,7 +42,12 @@ func Run() {
 	loader.Event, loader.Quest, loader.Part = "2025", 1, 1
 
 	data := loader.GetStrings()
-	part1 := get_name(data)
+	part1 := get_name(data, false)
 
-	fmt.Println(part1)
+	loader.Part = 2
+	data = loader.GetStrings()
+	part2 := get_name(data, true)
+
+	fmt.Printf("%s %s %s\n", part1, part2, "")
+
 }
--- 2025/quest1/quest1_test.go
+++ 2025/quest1/quest1_test.go
@@ -11,8 +11,23 @@ func Test1(t *testing.T) {
 		"R3,L2,R3,L1",
 	}
 
-	got := get_name(data)
+	got := get_name(data, false)
 	want := "Fyrryn"
+
+	if got != want {
+		t.Errorf("got %s, wanted %s", got, want)
+	}
+}
+
+func Test2(t *testing.T) {
+	data := []string{
+		"Vyrdax,Drakzyph,Fyrryn,Elarzris",
+		"",
+		"R3,L2,R3,L1",
+	}
+
+	got := get_name(data, true)
+	want := "Elarzris"
 
 	if got != want {
 		t.Errorf("got %s, wanted %s", got, want)
