92064888a59f2690f984db1f2a2bdd0c530502a6
David Bell[C <david.bell@gmail.com>
2025-11-04 00:22:23 +0000

    2025 quest 1 part 3 (with refactor)

--- 2025/quest1/quest1.go
+++ 2025/quest1/quest1.go
@@ -7,47 +7,78 @@ import (
 	"strings"
 )
 
-func get_name(data []string, circular bool) string {
+func parse_data(data []string) ([]string, []int) {
 	names := strings.Split(data[0], ",")
-	instructions := strings.Split(data[2], ",")
+	moves := []int{}
 
-	pos := 0
-	for _, ins := range instructions {
+	for _, ins := range strings.Split(data[2], ",") {
 		n, err := strconv.Atoi(ins[1:])
 		if err != nil {
 			panic(err)
 		}
 
 		if ins[0] == 'L' {
-			pos -= n
-		} else if ins[0] == 'R' {
-			pos += n
+			n *= -1
 		}
-		if circular {
-			pos = pos % len(names)
-			for pos < 0 {
-				pos += len(names)
-			}
-		} else if pos < 0 {
+
+		moves = append(moves, n)
+	}
+
+	return names, moves
+}
+
+func get_name(names []string, moves []int) string {
+	pos := 0
+	for _, move := range moves {
+		pos += move
+		if pos < 0 {
 			pos = 0
 		} else if pos >= len(names) {
 			pos = len(names) - 1
 		}
 	}
-
 	return names[pos]
+}
+
+func get_name_circular(names []string, moves []int) string {
+	pos := 0
+	for _, move := range moves {
+		pos = (pos + move) % len(names)
+		for pos < 0 {
+			pos += len(names)
+		}
+	}
+	return names[pos]
+}
+
+func get_name_with_swaps(names []string, moves []int) string {
+	for _, move := range moves {
+		move = move % len(names)
+		for move < 0 {
+			move += len(names)
+		}
+		names[0], names[move] = names[move], names[0]
+	}
+
+	return names[0]
 }
 
 func Run() {
 	loader.Event, loader.Quest, loader.Part = "2025", 1, 1
 
 	data := loader.GetStrings()
-	part1 := get_name(data, false)
+	names, moves := parse_data(data)
+	part1 := get_name(names, moves)
 
 	loader.Part = 2
 	data = loader.GetStrings()
-	part2 := get_name(data, true)
+	names, moves = parse_data(data)
+	part2 := get_name_circular(names, moves)
 
-	fmt.Printf("%s %s %s\n", part1, part2, "")
+	loader.Part = 3
+	data = loader.GetStrings()
+	names, moves = parse_data(data)
+	part3 := get_name_with_swaps(names, moves)
 
+	fmt.Printf("%s %s %s\n", part1, part2, part3)
 }
--- 2025/quest1/quest1_test.go
+++ 2025/quest1/quest1_test.go
@@ -11,7 +11,8 @@ func Test1(t *testing.T) {
 		"R3,L2,R3,L1",
 	}
 
-	got := get_name(data, false)
+	names, moves := parse_data(data)
+	got := get_name(names, moves)
 	want := "Fyrryn"
 
 	if got != want {
@@ -26,8 +27,25 @@ func Test2(t *testing.T) {
 		"R3,L2,R3,L1",
 	}
 
-	got := get_name(data, true)
+	names, moves := parse_data(data)
+	got := get_name_circular(names, moves)
 	want := "Elarzris"
+
+	if got != want {
+		t.Errorf("got %s, wanted %s", got, want)
+	}
+}
+
+func Test3(t *testing.T) {
+	data := []string{
+		"Vyrdax,Drakzyph,Fyrryn,Elarzris",
+		"",
+		"R3,L2,R3,L3",
+	}
+
+	names, moves := parse_data(data)
+	got := get_name_with_swaps(names, moves)
+	want := "Drakzyph"
 
 	if got != want {
 		t.Errorf("got %s, wanted %s", got, want)
