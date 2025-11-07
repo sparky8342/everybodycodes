9268d9b155fbfac89954a22407c4029adf883df2
David Bell[C <david.bell@gmail.com>
2025-11-07 23:59:16 +0000

    2025 quest 5 part 2

--- 2025/quest5/quest5.go
+++ 2025/quest5/quest5.go
@@ -3,6 +3,7 @@ package quest5
 import (
 	"fmt"
 	"loader"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -14,8 +15,8 @@ type Segment struct {
 	child *Segment
 }
 
-func parse_data(data []byte) []int {
-	parts := strings.Split(string(data), ":")
+func parse_line(line string) []int {
+	parts := strings.Split(line, ":")
 	num_strs := strings.Split(parts[1], ",")
 	nums := make([]int, len(num_strs))
 	for i, str := range num_strs {
@@ -40,30 +41,61 @@ func add_value(segment *Segment, value int) {
 	}
 }
 
-func quality(nums []int) string {
+func digits(n int) int {
+	d := 0
+	for n > 0 {
+		n /= 10
+		d++
+	}
+	return d
+}
+
+func quality(nums []int) int {
 	fishbone := &Segment{value: nums[0], left: -1, right: -1}
 
 	for i := 1; i < len(nums); i++ {
 		add_value(fishbone, nums[i])
 	}
 
-	qual := []string{}
+	qual := 0
 
 	segment := fishbone
 	for segment != nil {
-		qual = append(qual, strconv.Itoa(segment.value))
+		for i := 0; i < digits(segment.value); i++ {
+			qual *= 10
+		}
+		qual += segment.value
 		segment = segment.child
 	}
 
-	return strings.Join(qual, "")
+	return qual
+}
+
+func compare_quality(data []string) int {
+	min := math.MaxInt64
+	max := 0
+	for _, line := range data {
+		nums := parse_line(line)
+		qual := quality(nums)
+		if qual < min {
+			min = qual
+		} else if qual > max {
+			max = qual
+		}
+	}
+	return max - min
 }
 
 func Run() {
 	loader.Event, loader.Quest, loader.Part = "2025", 5, 1
 
 	data := loader.GetOneLine()
-	nums := parse_data(data)
+	nums := parse_line(string(data))
 	part1 := quality(nums)
 
-	fmt.Printf("%s %d %d\n", part1, 0, 0)
+	loader.Part = 2
+	data_part2 := loader.GetStrings()
+	part2 := compare_quality(data_part2)
+
+	fmt.Printf("%d %d %d\n", part1, part2, 0)
 }
--- 2025/quest5/quest5_test.go
+++ 2025/quest5/quest5_test.go
@@ -5,13 +5,35 @@ import (
 )
 
 func Test1(t *testing.T) {
-	data := []byte("58:5,3,7,8,9,10,4,5,7,8,8")
+	data := "58:5,3,7,8,9,10,4,5,7,8,8"
 
-	nums := parse_data(data)
+	nums := parse_line(data)
 	got := quality(nums)
-	want := "581078"
+	want := 581078
 
 	if got != want {
-		t.Errorf("got %s, wanted %s", got, want)
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func Test2(t *testing.T) {
+	data := []string{
+		"1:2,4,1,1,8,2,7,9,8,6",
+		"2:7,9,9,3,8,3,8,8,6,8",
+		"3:4,7,6,9,1,8,3,7,2,2",
+		"4:6,4,2,1,7,4,5,5,5,8",
+		"5:2,9,3,8,3,9,5,2,1,4",
+		"6:2,4,9,6,7,4,1,7,6,8",
+		"7:2,3,7,6,2,2,4,1,4,2",
+		"8:5,1,5,6,8,3,1,8,3,9",
+		"9:5,7,7,3,7,2,3,8,6,7",
+		"10:4,1,9,3,8,5,4,3,5,5",
+	}
+
+	got := compare_quality(data)
+	want := 77053
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
 	}
 }
