fc63a19d593cc1ba7ba1ce1c22c9b9ca83b30987
David Bell[C <david.bell@gmail.com>
2025-11-10 23:53:57 +0000

    2025 quest 6 part 3

--- 2025/quest6/quest6.go
+++ 2025/quest6/quest6.go
@@ -40,6 +40,69 @@ func total_pairs(data []byte) int {
 	return pairs
 }
 
+func total_pairs_in_range(data []byte, repeat int, dist int) int {
+	if repeat > 1 {
+		r := []byte{}
+		for i := 0; i < repeat; i++ {
+			r = append(r, data...)
+		}
+		data = r
+	}
+
+	pairs := 0
+
+	A, B, C := 0, 0, 0
+	for i := 0; i <= dist; i++ {
+		switch data[i] {
+		case 'A':
+			A++
+		case 'B':
+			B++
+		case 'C':
+			C++
+		}
+	}
+
+	p1, p2 := 0, dist
+
+	for _, b := range data {
+		switch b {
+		case 'a':
+			pairs += A
+		case 'b':
+			pairs += B
+		case 'c':
+			pairs += C
+		}
+
+		if p2 < len(data)-1 {
+			p2++
+			switch data[p2] {
+			case 'A':
+				A++
+			case 'B':
+				B++
+			case 'C':
+				C++
+			}
+		}
+
+		if p2 > dist*2 {
+			switch data[p1] {
+			case 'A':
+				A--
+			case 'B':
+				B--
+			case 'C':
+				C--
+			}
+			p1++
+		}
+	}
+
+	return pairs
+}
+
 func Run() {
 	loader.Event, loader.Quest, loader.Part = "2025", 6, 1
 
@@ -50,5 +113,9 @@ func Run() {
 	data = loader.GetOneLine()
 	part2 := total_pairs(data)
 
-	fmt.Printf("%d %d %d\n", part1, part2, 0)
+	loader.Part = 3
+	data = loader.GetOneLine()
+	part3 := total_pairs_in_range(data, 1000, 1000)
+
+	fmt.Printf("%d %d %d\n", part1, part2, part3)
 }
--- 2025/quest6/quest6_test.go
+++ 2025/quest6/quest6_test.go
@@ -25,3 +25,36 @@ func Test2(t *testing.T) {
 		t.Errorf("got %d, wanted %d", got, want)
 	}
 }
+
+func Test3(t *testing.T) {
+	data := []byte("AABCBABCABCabcabcABCCBAACBCa")
+
+	got := total_pairs_in_range(data, 1, 10)
+	want := 34
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func Test4(t *testing.T) {
+	data := []byte("AABCBABCABCabcabcABCCBAACBCa")
+
+	got := total_pairs_in_range(data, 2, 10)
+	want := 72
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func Test5(t *testing.T) {
+	data := []byte("AABCBABCABCabcabcABCCBAACBCa")
+
+	got := total_pairs_in_range(data, 1000, 1000)
+	want := 3442321
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
