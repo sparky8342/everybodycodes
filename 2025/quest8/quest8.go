a2dbcc35034c71fa95ea7c61decf26b8ec9471bc
David Bell[C <david.bell@gmail.com>
2025-11-12 23:55:04 +0000

    2025 quest 8 part 2

--- 2025/quest8/quest8.go
+++ 2025/quest8/quest8.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+type pair [2]int
+
 func parse_data(data []byte) []int {
 	num_strs := strings.Split(string(data), ",")
 	nums := make([]int, len(num_strs))
@@ -35,6 +37,34 @@ func centre_count(nails int, nums []int) int {
 	return count
 }
 
+func knots(nails int, nums []int) int {
+	lines := []pair{}
+	for i := 0; i < len(nums)-1; i++ {
+		a, b := nums[i], nums[i+1]
+		if a > b {
+			a, b = b, a
+		}
+		lines = append(lines, pair{a, b})
+	}
+
+	count := 0
+	for i := 0; i < len(lines); i++ {
+		a, b := lines[i][0], lines[i][1]
+
+		for j := i + 1; j < len(lines); j++ {
+			c, d := lines[j][0], lines[j][1]
+			if (c < a || c > b) && (d > a && d < b) {
+				count++
+			}
+			if (d < a || d > b) && (c > a && c < b) {
+				count++
+			}
+		}
+	}
+
+	return count
+}
+
 func Run() {
 	loader.Event, loader.Quest, loader.Part = "2025", 8, 1
 
@@ -42,5 +72,10 @@ func Run() {
 	nums := parse_data(data)
 	part1 := centre_count(32, nums)
 
-	fmt.Printf("%d %s %s\n", part1, "", "")
+	loader.Part = 2
+	data = loader.GetOneLine()
+	nums = parse_data(data)
+	part2 := knots(32, nums)
+
+	fmt.Printf("%d %d %s\n", part1, part2, "")
 }
--- 2025/quest8/quest8_test.go
+++ 2025/quest8/quest8_test.go
@@ -14,3 +14,14 @@ func Test1(t *testing.T) {
 		t.Errorf("got %d, wanted %d", got, want)
 	}
 }
+
+func Test2(t *testing.T) {
+	nums := []int{1, 5, 2, 6, 8, 4, 1, 7, 3, 5, 7, 8, 2}
+
+	got := knots(8, nums)
+	want := 21
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
