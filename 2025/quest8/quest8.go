55ffb8e5ddad51dbae468613a301ced7e36b1b2b
David Bell[C <david.bell@gmail.com>
2025-11-13 00:33:51 +0000

    simplified part 1 to use the lines slice

--- 2025/quest8/quest8.go
+++ 2025/quest8/quest8.go
@@ -9,10 +9,9 @@ import (
 
 type pair [2]int
 
-func parse_data(data []byte) ([]int, []pair) {
+func parse_data(data []byte) []pair {
 	num_strs := strings.Split(string(data), ",")
 	nums := make([]int, len(num_strs))
-
 	for i, str := range num_strs {
 		n, err := strconv.Atoi(str)
 		if err != nil {
@@ -30,18 +29,14 @@ func parse_data(data []byte) ([]int, []pair) {
 		lines = append(lines, pair{a, b})
 	}
 
-	return nums, lines
+	return lines
 }
 
-func centre_count(nails int, nums []int) int {
+func centre_count(nails int, lines []pair) int {
 	half := nails / 2
 	count := 0
-	for i := 1; i < len(nums); i++ {
-		a, b := nums[i-1], nums[i]
-		if a > b {
-			a, b = b, a
-		}
-		if a+half == b {
+	for _, line := range lines {
+		if line[1]-line[0] == half {
 			count++
 		}
 	}
@@ -100,17 +95,17 @@ func Run() {
 	loader.Event, loader.Quest, loader.Part = "2025", 8, 1
 
 	data := loader.GetOneLine()
-	nums, _ := parse_data(data)
-	part1 := centre_count(32, nums)
+	lines := parse_data(data)
+	part1 := centre_count(32, lines)
 
 	loader.Part = 2
 	data = loader.GetOneLine()
-	_, lines := parse_data(data)
+	lines = parse_data(data)
 	part2 := knots(32, lines)
 
 	loader.Part = 3
 	data = loader.GetOneLine()
-	_, lines = parse_data(data)
+	lines = parse_data(data)
 	part3 := best_cut(256, lines)
 
 	fmt.Printf("%d %d %d\n", part1, part2, part3)
--- 2025/quest8/quest8_test.go
+++ 2025/quest8/quest8_test.go
@@ -7,8 +7,8 @@ import (
 func Test1(t *testing.T) {
 	data := []byte("1,5,2,6,8,4,1,7,3")
 
-	nums, _ := parse_data(data)
-	got := centre_count(8, nums)
+	lines := parse_data(data)
+	got := centre_count(8, lines)
 	want := 4
 
 	if got != want {
@@ -19,7 +19,7 @@ func Test1(t *testing.T) {
 func Test2(t *testing.T) {
 	data := []byte("1,5,2,6,8,4,1,7,3,5,7,8,2")
 
-	_, lines := parse_data(data)
+	lines := parse_data(data)
 	got := knots(8, lines)
 	want := 21
 
@@ -31,7 +31,7 @@ func Test2(t *testing.T) {
 func Test3(t *testing.T) {
 	data := []byte("1,5,2,6,8,4,1,7,3,6")
 
-	_, lines := parse_data(data)
+	lines := parse_data(data)
 	got := best_cut(8, lines)
 	want := 7
 
