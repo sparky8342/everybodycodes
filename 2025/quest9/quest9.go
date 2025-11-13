ecefa1937c99f40c154ca88350cdcfd78435a557
David Bell[C <david.bell@gmail.com>
2025-11-13 23:52:22 +0000

    2025 quest 9 part 2

--- 2025/quest9/quest9.go
+++ 2025/quest9/quest9.go
@@ -3,16 +3,18 @@ package quest9
 import (
 	"fmt"
 	"loader"
+	"strings"
 )
 
 func parse_data(data []string) []string {
 	for i := range data {
-		data[i] = data[i][2:]
+		parts := strings.Split(data[i], ":")
+		data[i] = parts[1]
 	}
 	return data
 }
 
-func similarity(sequences []string) int {
+func similarity_part1(sequences []string) int {
 	parent1 := sequences[0]
 	parent2 := sequences[1]
 	child := sequences[2]
@@ -31,13 +33,59 @@ func similarity(sequences []string) int {
 	return p1_matches * p2_matches
 }
 
+func similarity(child string, parent1 string, parent2 string) int {
+	p1_matches := 0
+	p2_matches := 0
+
+	for i := 0; i < len(child); i++ {
+		if child[i] != parent1[i] && child[i] != parent2[i] {
+			return 0
+		}
+		if child[i] == parent1[i] {
+			p1_matches++
+		}
+		if child[i] == parent2[i] {
+			p2_matches++
+		}
+	}
+
+	return p1_matches * p2_matches
+}
+
+func similarity_sum(sequences []string) int {
+	sum := 0
+
+	for i := 0; i < len(sequences); i++ {
+		child := sequences[i]
+
+		for j := 0; j < len(sequences); j++ {
+			for k := j + 1; k < len(sequences); k++ {
+				if j == i || k == i {
+					continue
+				}
+
+				parent1 := sequences[j]
+				parent2 := sequences[k]
+
+				sum += similarity(child, parent1, parent2)
+			}
+		}
+	}
+
+	return sum
+}
+
 func Run() {
 	loader.Event, loader.Quest, loader.Part = "2025", 9, 1
 
 	data := loader.GetStrings()
-
 	sequences := parse_data(data)
-	part1 := similarity(sequences)
+	part1 := similarity_part1(sequences)
 
-	fmt.Printf("%d %d %d\n", part1, 0, 0)
+	loader.Part = 2
+	data = loader.GetStrings()
+	sequences = parse_data(data)
+	part2 := similarity_sum(sequences)
+
+	fmt.Printf("%d %d %d\n", part1, part2, 0)
 }
--- 2025/quest9/quest9_test.go
+++ 2025/quest9/quest9_test.go
@@ -12,8 +12,28 @@ func Test1(t *testing.T) {
 	}
 
 	sequences := parse_data(data)
-	got := similarity(sequences)
+	got := similarity_part1(sequences)
 	want := 414
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func Test2(t *testing.T) {
+	data := []string{
+		"1:GCAGGCGAGTATGATACCCGGCTAGCCACCCC",
+		"2:TCTCGCGAGGATATTACTGGGCCAGACCCCCC",
+		"3:GGTGGAACATTCGAAAGTTGCATAGGGTGGTG",
+		"4:GCTCGCGAGTATATTACCGAACCAGCCCCTCA",
+		"5:GCAGCTTAGTATGACCGCCAAATCGCGACTCA",
+		"6:AGTGGAACCTTGGATAGTCTCATATAGCGGCA",
+		"7:GGCGTAATAATCGGATGCTGCAGAGGCTGCTG",
+	}
+
+	sequences := parse_data(data)
+	got := similarity_sum(sequences)
+	want := 1245
 
 	if got != want {
 		t.Errorf("got %d, wanted %d", got, want)
