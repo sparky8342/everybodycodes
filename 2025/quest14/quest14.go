3baeae3db641dbb2996ce65289e9714c0b286adb
David Bell[C <david.bell@gmail.com>
2025-11-21 00:14:42 +0000

    2025 quest 14 part 3

--- 2025/quest14/quest14.go
+++ 2025/quest14/quest14.go
@@ -3,6 +3,7 @@ package quest14
 import (
 	"fmt"
 	"loader"
+	"strings"
 )
 
 var width, height int
@@ -90,6 +91,74 @@ func steps(grid_str []string, amount int) int {
 	return total
 }
 
+func matches(grid [][]byte, match []string) bool {
+	for y := 0; y < 8; y++ {
+		for x := 0; x < 8; x++ {
+			if grid[y+13][x+13] != match[y][x] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func cache_key(grid [][]byte) string {
+	strs := make([]string, height)
+	for y := 0; y < height; y++ {
+		strs[y] = string(grid[y])
+	}
+	return strings.Join(strs, "")
+}
+
+func steps_matching(match []string, amount int) int {
+	height = 34
+	width = 34
+
+	grid := make([][]byte, height)
+	for y := 0; y < height; y++ {
+		grid[y] = make([]byte, width)
+		for x := 0; x < width; x++ {
+			grid[y][x] = '.'
+		}
+	}
+
+	first_match := ""
+	sequence_start := 0
+	sequence_end := 0
+
+	total := 0
+	for i := 0; i < amount; i++ {
+		grid = step(grid)
+		if matches(grid, match) {
+			key := cache_key(grid)
+			if first_match == "" {
+				first_match = key
+				sequence_start = i
+			} else if first_match == key {
+				sequence_end = i
+				break
+			}
+			total += count_active(grid)
+		}
+	}
+
+	sequence := sequence_end - sequence_start
+	left := amount - sequence_end
+	repeats := left / sequence
+	total *= repeats + 1
+	from := sequence_end + repeats*sequence
+
+	total += count_active(grid)
+	for i := from; i < amount; i++ {
+		grid = step(grid)
+		if matches(grid, match) {
+			total += count_active(grid)
+		}
+	}
+
+	return total
+}
+
 func Run() {
 	loader.Event, loader.Quest, loader.Part = "2025", 14, 1
 
@@ -100,5 +169,9 @@ func Run() {
 	grid = loader.GetStrings()
 	part2 := steps(grid, 2025)
 
-	fmt.Printf("%d %d %d\n", part1, part2, 0)
+	loader.Part = 3
+	grid = loader.GetStrings()
+	part3 := steps_matching(grid, 1000000000)
+
+	fmt.Printf("%d %d %d\n", part1, part2, part3)
 }
--- 2025/quest14/quest14_test.go
+++ 2025/quest14/quest14_test.go
@@ -21,3 +21,23 @@ func Test1(t *testing.T) {
 		t.Errorf("got %d, wanted %d", got, want)
 	}
 }
+
+func Test2(t *testing.T) {
+	grid := []string{
+		"#......#",
+		"..#..#..",
+		".##..##.",
+		"...##...",
+		"...##...",
+		".##..##.",
+		"..#..#..",
+		"#......#",
+	}
+
+	got := steps_matching(grid, 1000000000)
+	want := 278388552
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
