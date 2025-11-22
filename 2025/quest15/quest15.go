1eec0fdfe8d756877f3d07acfeca3b952272dbb5
David Bell[C <david.bell@gmail.com>
2025-11-22 09:28:17 +0000

    2025 quest 15 part 3 (slow and messy)

--- 2025/quest15/quest15.go
+++ 2025/quest15/quest15.go
@@ -1,8 +1,10 @@
 package quest15
 
 import (
+	"container/heap"
 	"fmt"
 	"loader"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -20,6 +22,38 @@ type Pos struct {
 type Entry struct {
 	pos      Pos
 	distance int
+}
+
+type Wall struct {
+	start    Pos
+	end      Pos
+	vertical bool
+}
+
+type PriorityQueue []*Entry
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].distance < pq[j].distance
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	item := x.(*Entry)
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil // avoid memory leak
+	*pq = old[0 : n-1]
+	return item
 }
 
 var cardinal_dirs [4][2]int
@@ -121,6 +155,197 @@ func find_exit(moves []Move) int {
 	return -1
 }
 
+func crosses_wall(pos Pos, wall Wall) bool {
+	if wall.vertical && wall.start.x == pos.x && pos.y >= wall.start.y && pos.y <= wall.end.y {
+		return true
+	}
+	if !wall.vertical && wall.start.y == pos.y && pos.x >= wall.start.x && pos.x <= wall.end.x {
+		return true
+	}
+	return false
+}
+
+func crosses_walls(pos Pos, walls []Wall) bool {
+	for _, wall := range walls {
+		if crosses_wall(pos, wall) {
+			return true
+		}
+	}
+	return false
+}
+
+func find_exit_large(moves []Move) int {
+	dirs := [4]byte{'U', 'R', 'D', 'L'}
+	current_dir := 0
+
+	walls := []Wall{}
+
+	x_walls := map[int][2]int{}
+	y_walls := map[int][2]int{}
+
+	x_turns := map[int]struct{}{}
+	y_turns := map[int]struct{}{}
+	x_turns[0] = struct{}{}
+	y_turns[0] = struct{}{}
+
+	start := Pos{}
+	pos := Pos{}
+
+	x_min, x_max, y_min, y_max := math.MaxInt64, 0, math.MaxInt64, 0
+
+	for _, move := range moves {
+		if move.dir == 'L' {
+			if current_dir == 0 {
+				current_dir = 3
+			} else {
+				current_dir--
+			}
+		} else if move.dir == 'R' {
+			if current_dir == 3 {
+				current_dir = 0
+			} else {
+				current_dir++
+			}
+		}
+
+		last_pos := pos
+		switch dirs[current_dir] {
+		case 'U':
+			pos.y -= move.amount
+		case 'R':
+			pos.x += move.amount
+		case 'D':
+			pos.y += move.amount
+		case 'L':
+			pos.x -= move.amount
+		}
+
+		wall := Wall{start: last_pos, end: pos}
+		if dirs[current_dir] == 'U' || dirs[current_dir] == 'D' {
+			wall.vertical = true
+		}
+		if wall.start.x > wall.end.x || wall.start.y > wall.end.y {
+			wall.start, wall.end = wall.end, wall.start
+		}
+		walls = append(walls, wall)
+
+		var vals [2]int
+		if dirs[current_dir] == 'U' || dirs[current_dir] == 'D' {
+			vals = [2]int{last_pos.y, pos.y}
+		} else if dirs[current_dir] == 'L' || dirs[current_dir] == 'R' {
+			vals = [2]int{last_pos.x, pos.x}
+		}
+
+		if vals[0] > vals[1] {
+			vals[0], vals[1] = vals[1], vals[0]
+		}
+
+		if dirs[current_dir] == 'U' || dirs[current_dir] == 'D' {
+			y_walls[pos.x] = vals
+		} else if dirs[current_dir] == 'L' || dirs[current_dir] == 'R' {
+			x_walls[pos.y] = vals
+		}
+
+		if pos.x > x_max {
+			x_max = pos.x
+		} else if pos.x < x_min {
+			x_min = pos.x
+		}
+		if pos.y > y_max {
+			y_max = pos.y
+		} else if pos.y < y_min {
+			y_min = pos.y
+		}
+
+		x_turns[pos.x-1] = struct{}{}
+		x_turns[pos.x] = struct{}{}
+		x_turns[pos.x+1] = struct{}{}
+		y_turns[pos.y-1] = struct{}{}
+		y_turns[pos.y] = struct{}{}
+		y_turns[pos.y+1] = struct{}{}
+	}
+
+	x_min -= 2
+	x_max += 2
+	y_min -= 2
+	y_max += 2
+
+	x_walls[y_min] = [2]int{x_min, x_max}
+	x_walls[y_max] = [2]int{x_min, x_max}
+	y_walls[x_min] = [2]int{y_min, y_max}
+	y_walls[x_max] = [2]int{y_min, y_max}
+
+	end := pos
+
+	visited := map[Pos]struct{}{}
+
+	queue := make(PriorityQueue, 1)
+	queue[0] = &Entry{pos: start}
+	heap.Init(&queue)
+
+	for queue.Len() > 0 {
+		entry := heap.Pop(&queue).(*Entry)
+
+		pos := entry.pos
+
+		if _, ok := visited[pos]; ok {
+			continue
+		}
+		visited[pos] = struct{}{}
+
+		if pos.x == end.x && pos.y == end.y {
+			return entry.distance
+		}
+
+		for _, dir := range cardinal_dirs {
+			dx, dy := dir[0], dir[1]
+
+			new_pos := pos
+			dist := 0
+			for {
+				new_pos.x += dx
+				new_pos.y += dy
+				dist++
+
+				if new_pos.x == end.x && new_pos.y == end.y {
+					return entry.distance + dist
+				}
+
+				if dy == 0 {
+					if _, ok := x_turns[new_pos.x]; ok {
+						if crosses_walls(new_pos, walls) {
+							new_pos.x -= dx
+							new_pos.y -= dy
+							dist--
+						}
+						break
+					}
+				}
+				if dx == 0 {
+					if _, ok := y_turns[new_pos.y]; ok {
+						if crosses_walls(new_pos, walls) {
+							new_pos.x -= dx
+							new_pos.y -= dy
+							dist--
+						}
+						break
+					}
+				}
+
+				if (dy == 0 && new_pos.x == end.x) || (dx == 0 && new_pos.y == end.y) {
+					break
+				}
+			}
+
+			new_dist := entry.distance + dist
+			heap.Push(&queue, &Entry{pos: new_pos, distance: new_dist})
+		}
+
+	}
+
+	return -1
+}
+
 func Run() {
 	loader.Event, loader.Quest, loader.Part = "2025", 15, 1
 
@@ -133,5 +358,10 @@ func Run() {
 	moves = parse_data(data)
 	part2 := find_exit(moves)
 
-	fmt.Printf("%d %d %d\n", part1, part2, 0)
+	loader.Part = 3
+	data = loader.GetOneLine()
+	moves = parse_data(data)
+	part3 := find_exit_large(moves)
+
+	fmt.Printf("%d %d %d\n", part1, part2, part3)
 }
