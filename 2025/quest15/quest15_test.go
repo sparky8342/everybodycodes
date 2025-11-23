3e38c49e3864047242332edbf434f7d0511b735a
David Bell[C <david.bell@gmail.com>
2025-11-23 21:43:53 +0000

    added test

--- 2025/quest15/quest15_test.go
+++ 2025/quest15/quest15_test.go
@@ -15,3 +15,15 @@ func Test1(t *testing.T) {
 		t.Errorf("got %d, wanted %d", got, want)
 	}
 }
+
+func Test2(t *testing.T) {
+	data := []byte("L6,L3,L6,R3,L6,L3,L3,R6,L6,R6,L6,L6,R3,L3,L3,R3,R3,L6,L6,L3")
+
+	moves := parse_data(data)
+	got := find_exit_large(moves)
+	want := 16
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
