376f318e194ce30590fcd89251861a17942e91a1
David Bell[C <david.bell@gmail.com>
2025-11-28 01:08:05 +0000

    2025 quest 19 part 2

--- 2025/quest19/quest19.go
+++ 2025/quest19/quest19.go
@@ -3,14 +3,26 @@ package quest19
 import (
 	"fmt"
 	"loader"
+	"math"
 	"strconv"
 	"strings"
 )
 
+type pair [2]int
+
 type Wall struct {
-	x          int
-	gap_bottom int
-	gap_top    int
+	x    int
+	gaps []pair
+}
+
+type Pos struct {
+	x int
+	y int
+}
+
+type Entry struct {
+	pos   Pos
+	flaps int
 }
 
 func parse_data(data []string) []Wall {
@@ -26,10 +38,21 @@ func parse_data(data []string) []Wall {
 			}
 			nums[i] = n
 		}
+
+		x := nums[0]
+		gap := pair{nums[1], nums[1] + nums[2] - 1}
+
+		if len(walls) > 0 {
+			last := len(walls) - 1
+			if walls[last].x == x {
+				walls[last].gaps = append(walls[last].gaps, gap)
+				continue
+			}
+		}
+
 		walls = append(walls, Wall{
-			x:          nums[0],
-			gap_bottom: nums[1],
-			gap_top:    nums[1] + nums[2] - 1,
+			x:    nums[0],
+			gaps: []pair{gap},
 		})
 	}
 
@@ -42,7 +65,7 @@ func flap(walls []Wall) int {
 	flaps := 0
 	for _, wall := range walls {
 		for ; x <= wall.x; x++ {
-			if y < wall.gap_bottom {
+			if y < wall.gaps[0][0] {
 				flaps++
 				y++
 			} else {
@@ -54,6 +77,58 @@ func flap(walls []Wall) int {
 	return flaps
 }
 
+func multi_flap(walls []Wall) int {
+	x_walls := map[int][]pair{}
+
+	// TODO - do this format in parse_data instead (and change part 1)
+	for _, wall := range walls {
+		if _, ok := x_walls[wall.x]; !ok {
+			x_walls[wall.x] = []pair{}
+		}
+		x_walls[wall.x] = append(x_walls[wall.x], wall.gaps...)
+	}
+
+	end_x := walls[len(walls)-1].x + 1
+
+	min_flaps := math.MaxInt32
+	search(Pos{}, end_x, 0, x_walls, &min_flaps)
+
+	return min_flaps
+}
+
+func search(pos Pos, end_x int, flaps int, x_walls map[int][]pair, min_flaps *int) {
+	if flaps > *min_flaps {
+		return
+	}
+
+	x := pos.x + 1
+
+	if x == end_x {
+		if flaps < *min_flaps {
+			*min_flaps = flaps
+		}
+		return
+	}
+
+	if gaps, ok := x_walls[x]; ok {
+		for _, y := range []int{pos.y - 1, pos.y + 1} {
+			for _, gap := range gaps {
+				if y >= gap[0] && y <= gap[1] {
+					new_flaps := flaps
+					if y == pos.y+1 {
+						new_flaps++
+					}
+					search(Pos{x: x, y: y}, end_x, new_flaps, x_walls, min_flaps)
+					break
+				}
+			}
+		}
+	} else {
+		search(Pos{x: x, y: pos.y - 1}, end_x, flaps, x_walls, min_flaps)
+		search(Pos{x: x, y: pos.y + 1}, end_x, flaps+1, x_walls, min_flaps)
+	}
+}
+
 func Run() {
 	loader.Event, loader.Quest, loader.Part = "2025", 19, 1
 
@@ -61,5 +136,10 @@ func Run() {
 	walls := parse_data(data)
 	part1 := flap(walls)
 
-	fmt.Printf("%d %d %d\n", part1, 0, 0)
+	loader.Part = 2
+	data = loader.GetStrings()
+	walls = parse_data(data)
+	part2 := multi_flap(walls)
+
+	fmt.Printf("%d %d %d\n", part1, part2, 0)
 }
--- 2025/quest19/quest19_test.go
+++ 2025/quest19/quest19_test.go
@@ -22,3 +22,24 @@ func Test1(t *testing.T) {
 		t.Errorf("got %d, wanted %d", got, want)
 	}
 }
+
+func Test2(t *testing.T) {
+	data := []string{
+		"7,7,2",
+		"7,1,3",
+		"12,0,4",
+		"15,5,3",
+		"24,1,6",
+		"28,5,5",
+		"40,3,3",
+		"40,8,2",
+	}
+
+	walls := parse_data(data)
+	got := multi_flap(walls)
+	want := 22
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
