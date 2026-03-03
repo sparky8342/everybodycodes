7d2400a9ed4fc63459469feefd34e75e1406806a
David Bell[C <david.bell@gmail.com>
2026-03-03 23:35:18 +0000

    Story 3 Quest 1 Part 2

--- stories/3_melody_made_of_code/quest1/quest1.go
+++ stories/3_melody_made_of_code/quest1/quest1.go
@@ -3,46 +3,88 @@ package quest1
 import (
 	"fmt"
 	"loader"
+	"sort"
 	"strconv"
 	"strings"
 )
 
+type Scale struct {
+	id    int
+	red   int
+	green int
+	blue  int
+	sum   int
+	shine int
+}
+
 func colour_value(colour string) int {
 	value := 0
 	for i := 5; i >= 0; i-- {
-		if colour[i] == 'R' || colour[i] == 'G' || colour[i] == 'B' {
+		if colour[i] == 'R' || colour[i] == 'G' || colour[i] == 'B' || colour[i] == 'S' {
 			value += 1 << (5 - i)
 		}
 	}
 	return value
 }
 
-func green_dominant(data []string) int {
-	total := 0
+func parse_data(data []string) []Scale {
+	scales := make([]Scale, len(data))
 
-	for _, line := range data {
+	for i, line := range data {
 		parts := strings.Split(line, ":")
+
 		id, err := strconv.Atoi(parts[0])
 		if err != nil {
 			panic(err)
 		}
+		scales[i].id = id
+
 		parts2 := strings.Split(parts[1], " ")
-		red := colour_value(parts2[0])
-		green := colour_value(parts2[1])
-		blue := colour_value(parts2[2])
-		if green > red && green > blue {
-			total += id
+		scales[i].red = colour_value(parts2[0])
+		scales[i].green = colour_value(parts2[1])
+		scales[i].blue = colour_value(parts2[2])
+		scales[i].sum = scales[i].red + scales[i].green + scales[i].blue
+
+		if len(parts2) == 4 {
+			scales[i].shine = colour_value(parts2[3])
 		}
 	}
 
+	return scales
+}
+
+func green_dominant(scales []Scale) int {
+	total := 0
+	for _, scale := range scales {
+		if scale.green > scale.red && scale.green > scale.blue {
+			total += scale.id
+		}
+	}
 	return total
+}
+
+func darkest_shiny(scales []Scale) int {
+	sort.Slice(scales, func(i, j int) bool {
+		if scales[i].shine == scales[j].shine {
+			return scales[i].sum < scales[j].sum
+		} else {
+			return scales[i].shine > scales[j].shine
+		}
+	})
+	return scales[0].id
 }
 
 func Run() {
 	loader.Event, loader.Quest, loader.Part = "3", 1, 1
 
 	data := loader.GetStrings()
-	part1 := green_dominant(data)
+	scales := parse_data(data)
+	part1 := green_dominant(scales)
 
-	fmt.Printf("%d %d %d\n", part1, -1, -1)
+	loader.Part = 2
+	data = loader.GetStrings()
+	scales = parse_data(data)
+	part2 := darkest_shiny(scales)
+
+	fmt.Printf("%d %d %d\n", part1, part2, -1)
 }
--- stories/3_melody_made_of_code/quest1/quest1_test.go
+++ stories/3_melody_made_of_code/quest1/quest1_test.go
@@ -12,8 +12,26 @@ func Test1(t *testing.T) {
 		"6710:rrrRRr ggGGGg bbBBbB",
 	}
 
-	got := green_dominant(data)
+	scales := parse_data(data)
+	got := green_dominant(scales)
 	want := 9166
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func Test2(t *testing.T) {
+	data := []string{
+		"2456:rrrrrr ggGgGG bbbbBB sSsSsS",
+		"7689:rrRrrr ggGggg bbbBBB ssSSss",
+		"3145:rrRrRr gggGgg bbbbBB sSsSsS",
+		"6710:rrrRRr ggGGGg bbBBbB ssSSss",
+	}
+
+	scales := parse_data(data)
+	got := darkest_shiny(scales)
+	want := 2456
 
 	if got != want {
 		t.Errorf("got %d, wanted %d", got, want)
