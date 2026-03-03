f1ac0984d9199645e48d5060e119c26c5dd738a5
David Bell[C <david.bell@gmail.com>
2026-03-03 23:55:27 +0000

    Story 3 Quest 1 Part 3

--- stories/3_melody_made_of_code/quest1/quest1.go
+++ stories/3_melody_made_of_code/quest1/quest1.go
@@ -74,6 +74,51 @@ func darkest_shiny(scales []Scale) int {
 	return scales[0].id
 }
 
+func largest_group(scales []Scale) int {
+	scale_types := map[string][]int{}
+
+	for _, scale := range scales {
+		var colour byte
+		if scale.red > scale.green && scale.red > scale.blue {
+			colour = 'R'
+		} else if scale.green > scale.red && scale.green > scale.blue {
+			colour = 'G'
+		} else if scale.blue > scale.red && scale.blue > scale.green {
+			colour = 'B'
+		} else {
+			continue
+		}
+
+		var shine byte
+		if scale.shine <= 30 {
+			shine = 'M'
+		} else if scale.shine >= 33 {
+			shine = 'G'
+		} else {
+			continue
+		}
+
+		typ := string([]byte{colour, shine})
+		scale_types[typ] = append(scale_types[typ], scale.id)
+	}
+
+	best := 0
+	best_type := ""
+	for key, value := range scale_types {
+		if len(value) > best {
+			best = len(value)
+			best_type = key
+		}
+	}
+
+	total := 0
+	for _, id := range scale_types[best_type] {
+		total += id
+	}
+
+	return total
+}
+
 func Run() {
 	loader.Event, loader.Quest, loader.Part = "3", 1, 1
 
@@ -86,5 +131,10 @@ func Run() {
 	scales = parse_data(data)
 	part2 := darkest_shiny(scales)
 
-	fmt.Printf("%d %d %d\n", part1, part2, -1)
+	loader.Part = 3
+	data = loader.GetStrings()
+	scales = parse_data(data)
+	part3 := largest_group(scales)
+
+	fmt.Printf("%d %d %d\n", part1, part2, part3)
 }
--- stories/3_melody_made_of_code/quest1/quest1_test.go
+++ stories/3_melody_made_of_code/quest1/quest1_test.go
@@ -37,3 +37,32 @@ func Test2(t *testing.T) {
 		t.Errorf("got %d, wanted %d", got, want)
 	}
 }
+
+func Test3(t *testing.T) {
+	data := []string{
+		"15437:rRrrRR gGGGGG BBBBBB sSSSSS",
+		"94682:RrRrrR gGGggG bBBBBB ssSSSs",
+		"56513:RRRrrr ggGGgG bbbBbb ssSsSS",
+		"76346:rRRrrR GGgggg bbbBBB ssssSs",
+		"87569:rrRRrR gGGGGg BbbbbB SssSss",
+		"44191:rrrrrr gGgGGG bBBbbB sSssSS",
+		"49176:rRRrRr GggggG BbBbbb sSSssS",
+		"85071:RRrrrr GgGGgg BBbbbb SSsSss",
+		"44303:rRRrrR gGggGg bBbBBB SsSSSs",
+		"94978:rrRrRR ggGggG BBbBBb SSSSSS",
+		"26325:rrRRrr gGGGgg BBbBbb SssssS",
+		"43463:rrrrRR gGgGgg bBBbBB sSssSs",
+		"15059:RRrrrR GGgggG bbBBbb sSSsSS",
+		"85004:RRRrrR GgGgGG bbbBBB sSssss",
+		"56121:RRrRrr gGgGgg BbbbBB sSsSSs",
+		"80219:rRRrRR GGGggg BBbbbb SssSSs",
+	}
+
+	scales := parse_data(data)
+	got := largest_group(scales)
+	want := 292320
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
